7155508b78eda3a78349fbd2652b4eae9d9aed6e
Kasia Kowalska <k.kowalska68@yahoo.com>
2018-03-16 09:52:23 +0100

    Modify Column.js

--- Column.js
+++ Column.js
@@ -29,7 +29,7 @@ function Column(id, name) {
 				data: {
 				name: cardName,
 				bootcamp_kanban_column_id: self.id	
-				}
+				},
 				success: function(response) {
 					var card = new Card(response.id, cardName);
 					self.createCard(card);
